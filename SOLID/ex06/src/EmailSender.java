1d9214edbb462ae492a1fa11c55f83e626fae095
Payel-Manna <mannapayel800@gmail.com>
2026-02-22 23:07:11 +0530

    Refactored EX06

--- SOLID/ex06/src/EmailSender.java
+++ SOLID/ex06/src/EmailSender.java
@@ -2,8 +2,8 @@ public class EmailSender extends NotificationSender {
     public EmailSender(AuditLog audit) { super(audit); }
 
     @Override
-    public void send(Notification n) {
-        // LSP smell: truncates silently, changing meaning
+    protected void doSend(Notification n) {
+        // LSP smell: truncates silently, changing meaning->Fixed
         String body = n.body;
         if (body.length() > 40) body = body.substring(0, 40);
         System.out.println("EMAIL -> to=" + n.email + " subject=" + n.subject + " body=" + body);
